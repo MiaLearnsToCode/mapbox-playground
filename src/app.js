8bb3d60004260ab8abe116ae06c5a0c780e2fd8c
MiaLearnsToCode <meroimia@gmail.com>
2019-10-09 14:09:37 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bulma'
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Mapbox</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
