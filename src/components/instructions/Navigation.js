296fb2aec31900730ad5fbdf420aee53139f2767
MiaLearnsToCode <meroimia@gmail.com>
2019-10-14 15:22:22 +0100

    instructions

--- src/components/instructions/Navigation.js
+++ src/components/instructions/Navigation.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => (
-  <div>
-    Navigation
-  </div>
+  <section className="hero is-fullheight navigation">
+    <div className="tile is-ancestor">
+      <div className="tile is-4 is-vertical is-parent">
+        <div className="tile is-child box">
+          <p className="title">Move</p>
+          <p className="emoji-nav">⬅️ ➡️</p>
+          <p className="emoji-nav">⬆️ ⬇️</p>
+        </div>
+        <div className="tile is-child box">
+          <p className="title">Zoom</p>
+          <p className="emoji-nav">🔍</p>
+        </div>
+      </div>
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <p className="title">Navigation </p>
+          <p>Use the key board arrows to move around the map.</p>
+          <p>Slide two fingers up or down to zoom, you have to be zoomed in in order to gain points </p>
+          <Link className="button" to="/rotation">next</Link>
+        </div>
+      </div>
+    </div>
+  </section>
 )
 
 export default Navigation
--- src/components/instructions/Rotation.js
+++ src/components/instructions/Rotation.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Rotation = () => (
-  <div>
-    Rotation
-  </div>
+  <section className="hero is-fullheight navigation">
+    <div className="tile is-ancestor">
+      <div className="tile is-4 is-vertical is-parent">
+        <div className="tile is-child box">
+          <p className="title">Rotate</p>
+          <p className="emoji-nav">⬅️ ➡️</p>
+          
+        </div>
+        <div className="tile is-child box">
+          <p className="title">Angle</p>
+          <p className="emoji-nav">⬆️ ⬇️</p>
+        </div>
+      </div>
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <p className="title">Rotation</p>
+          <p>If you hold cmd + arrow you can rotate or change the angle view in the game.</p>
+          <p>With the left and right arrows you can rotate</p>
+          <p>With the up and down arrows you can change the angle view</p>
+          <Link className="button" to="/navigation">back</Link>
+          <Link className="button" to="/characters">next</Link>
+        </div>
+      </div>
+    </div>
+  </section>
 )
 
 export default Rotation
